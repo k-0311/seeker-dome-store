80f348e79f39994f46642b46b1722faa532f4254
Seeker <Seeker@mogul-tech.com>
2021-11-24 11:18:05 +0800

    xxxxxxxx

--- Fe2O3/functions/src/main.rs
+++ Fe2O3/functions/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    loop_fn(fibo(8));
+    range_fn();
+    f_c_rev(80, "f");
+}
+
+fn loop_fn(mut x: u32) {
+    let res = loop {
+        x = x / 2;
+        println!("x: {}", x);
+        if x < 10 {
+            break x * x;
+        }
+    };
+    println!("{}", res)
+}
+
+fn fibo(mut n: u8) -> u32 {
+    let mut x = 1;
+    let mut y = 1;
+    while n > 0 {
+        let temp = y;
+        y = x + y;
+        x = temp;
+        n = n - 1;
+        println!("{}", y);
+    }
+    return y;
+}
+
+fn range_fn() {
+    for number in (1..8).rev() {
+        println!("number: {}", number)
+    }
+}
+
+fn f_c_rev(v: i32, target: &str) -> i32 {
+    const DIFF: i32 = 32;
+    return if target == "c" {
+        5 * (v - DIFF) / 9
+    } else {
+        9 * v / 5 + DIFF
+    };
 }
--- Fe2O3/variables/src/main.rs
+++ Fe2O3/variables/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let x = "    ";
+    let x = "       ";
     let x = x.len();
     // let a: u32 = "42".parse().expect("aaaa");
     let tup = (20, "a", 3.14);
     let a = tup.1;
     let b = tup.2;
     let c = tup.0;
-    println!("{},{},{}", a, b, c);
+    println!("{} {} {} {}", x, a, b, c);
 }
